773cf047b760f9688611e3f5a21435d59494eb82
kevinzhou2015 <xll_kgg@yeah.net>
2018-08-30 23:21:15 +0800

    init

---
+++ app/src/main/java/kevin/com/nanal/MapsActivity.kt
@@ -0,0 +1,16 @@
+package kevin.com.nanal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    }
+
+
+}
