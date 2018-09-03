bea1c1f50c0703141846603ddf163402f8065276
kevinzhou2015 <xll_kgg@yeah.net>
2018-09-04 02:18:09 +0800

    配置leancloud使用cipher加密app key

--- app/src/main/java/kevin/com/nanal/NAnalApplication.kt
+++ app/src/main/java/kevin/com/nanal/NAnalApplication.kt
@@ -1,10 +1,13 @@
 package kevin.com.nanal
 
 import android.app.Application
+import com.avos.avoscloud.AVOSCloud
 import com.baidu.mapapi.CoordType
 import com.baidu.mapapi.SDKInitializer
 import com.facebook.stetho.Stetho
-import kevin.com.nanal.data.AppDatabase
+import net.idik.lib.cipher.so.CipherClient
+
+
 
 /**
  * Created on 31/08/2018 by kevin.
@@ -13,10 +16,13 @@ import kevin.com.nanal.data.AppDatabase
 class NAnalApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        SDKInitializer.initialize(this);
+        SDKInitializer.initialize(this)
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
-        SDKInitializer.setCoordType(CoordType.BD09LL);
-        Stetho.initializeWithDefaults(this);
+        SDKInitializer.setCoordType(CoordType.BD09LL)
+        Stetho.initializeWithDefaults(this)
+
+        AVOSCloud.initialize(this,CipherClient.leancloudId(),CipherClient.leancloudKey())
+
     }
 }
